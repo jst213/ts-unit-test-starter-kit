35d72e1bfa97e3291846751afa058683b632c876
Jeremy Santoso Tanasaleh <jtanasaleh@student.unimelb.edu.au>
2024-08-21 13:52:10 +1000

    Testing

--- tests/ecommerce.test.ts
+++ tests/ecommerce.test.ts
@@ -4,6 +4,7 @@ import {
   addToCart,
   calculateTotal,
   clearCart,
+  applyShippingDiscount,
 } from "../src/ecommerce";
 
 let cart = {};
@@ -15,9 +16,49 @@ describe("E-commerce System", () => {
   });
 
   it("should calculate price of all products", () => {
+    //Arrange
     addToCart("Soap", 2);
     addToCart("Shampoo", 2);
+
+    //Act
     const sum = calculateTotal();
+
+    //Assert
     expect(sum).toBe(600);
   });
+
+  it("should add items to cart", () => {
+    //Arrange
+    let cart = {};
+
+    //Act
+    cart = addToCart("Soap", 2)
+
+    //Assert
+    expect(cart["Soap"]).toBe(2);
+  })
+
+  it("should apply shipping discount", () => {
+    let cart = {};
+
+    addToCart("Soap", 2);
+    addToCart("Shampoo", 2);
+
+    const sum = calculateTotal();
+
+    expect(applyShippingDiscount(sum)).toBe(590);
+
+  })
+
+  it("should not apply shipping discount", () => {
+    let cart = {};
+
+    addToCart("Soap", 1);
+    addToCart("Shampoo", 1);
+
+    const sum = calculateTotal();
+
+    expect(applyShippingDiscount(sum)).toBe(300);
+
+  })
 });
